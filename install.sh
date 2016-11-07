9d3bcfcbce90e98aa3836101d16c3165a3d4e357
cookie-s <cookie-s@users.noreply.github.com>
2016-11-08 04:33:23 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 
 mkdir -p ~/.vim ~/.vimbackup
 
-ln -s dotfiles/.bashrc \
-    dotfiles/.gdbinit \
-    dotfiles/.gitconfig \
-    dotfiles/.tmux.conf \
-    dotfiles/.vimrc \
-    dotfiles/.zshenv \
-    dotfiles/.zshrc \
-    dotfiles/.zlogin \
+ln -s ~/dotfiles/.bashrc \
+    ~/dotfiles/.gdbinit \
+    ~/dotfiles/.gitconfig \
+    ~/dotfiles/.tmux.conf \
+    ~/dotfiles/.vimrc \
+    ~/dotfiles/.zshenv \
+    ~/dotfiles/.zshrc \
+    ~/dotfiles/.zlogin \
     ~
-ln -s dotfiles/.vim/indent ~/.vim/
+ln -s ~/dotfiles/.vim/indent ~/.vim/
