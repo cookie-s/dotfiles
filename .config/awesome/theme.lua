88bb14b44c06b42a004272e3229c4c840b5d0b0d
kcz <kcz@kcz.sh>
2025-01-20 09:34:51 -0500

    awesome: remove old cfg

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -73,7 +73,6 @@ theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/defau
 theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
 
 theme.wallpaper = "/usr/share/awesome/themes/default/background.png"
--- theme.wallpaper = "/home/cookies/.config/awesome/satori/201104082338496ae.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
